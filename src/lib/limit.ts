5442273bda92e38f0d681349f7bb198efef7b738
Matt Bremmers <bremmdev@gmail.com>
2024-02-24 12:50:12 +0100

    refactor rate limit

--- src/lib/limit.ts
+++ src/lib/limit.ts
@@ -1,4 +1,6 @@
 import { headers } from "next/headers";
+import { getClient } from "@/lib/client";
+import { randomUUID } from "crypto";
 
 export function getIp() {
   let forwardedFor = headers().get("x-forwarded-for");
@@ -21,4 +23,48 @@ export function createSlidingWindow(timeWindow: number) {
     .toISOString()
     .replace("Z", "0000Z");
   return timeWindowStart;
+}
+
+export async function checkRateLimit(limit: number) {
+  const ip =
+    process.env.ENVIRONMENT === "dev" ? "0.0.0.0" : getIp() || "unknown";
+  const tableClient = getClient();
+
+  try {
+    const timeWindowStart = createSlidingWindow(60);
+    const entities = tableClient.listEntities({
+      queryOptions: {
+        //note: cast to datetime is required for the query to work
+        filter: `PartitionKey eq '${ip}' and Timestamp ge datetime'${timeWindowStart}'`,
+      },
+    });
+    //count the current request and the previous ones in the time window
+    let count = 1;
+    for await (const _ of entities) {
+      count++;
+    }
+
+    //check if the limit is exceeded
+    if (count > limit) {
+      return {
+        limitExceeded: true,
+        ip,
+        count,
+      };
+    }
+
+    //no limit exceeded, create a new entity for the ip
+    tableClient.createEntity({
+      partitionKey: ip,
+      rowKey: randomUUID(),
+    });
+
+    return {
+      limitExceeded: false,
+      ip,
+      count,
+    };
+  } catch (e) {
+    throw new Error("Internal server error");
+  }
 }
